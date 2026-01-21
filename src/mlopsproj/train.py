4009db8ebd8206b70b0b5e572ff1090aa3500936
Lukas Tej Marner <s215167@dtu.dk>
2026-01-21 15:35:05 +0100

    Update config path in train.py

--- src/mlopsproj/train.py
+++ src/mlopsproj/train.py
@@ -157,7 +157,7 @@ def create_profiler(profiler_type: Optional[str], output_dir: Path):
 
     return None
 
-@hydra.main(version_base="1.3", config_path="configs", config_name="config")
+@hydra.main(version_base="1.3", config_path="../../configs", config_name="config")
 def main(cfg: DictConfig):
     # Create output directory
     output_dir = Path(cfg.model.output_dir)
